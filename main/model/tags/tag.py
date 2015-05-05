e20b294db4222508a83367cde1d536235efbd710
Tobias Burgherr <tburgherr@gmail.com>
2015-05-05 09:26:38 +0200

    Change tag variable to private

--- main/model/tags/tag.py
+++ main/model/tags/tag.py
@@ -4,22 +4,22 @@ from __future__ import absolute_import
 
 class Tag(object):
   def __init__(self, name, style, group=[], status="LOCAL", counter=1):
-    self.name = name
-    self.style = style
-    self.group = group
-    self.status = status
-    self.counter = counter
+    self._name = name
+    self._style = style
+    self._group = group
+    self._status = status
+    self._counter = counter
 
   def get_name(self):
-    return self.name
+    return self._name
 
   def set_name(self, name):
-    self.name = name
-    return self.name
+    self._name = name
+    return self._name
 
   def set_style(self, style, group):
-    self.style = style
-    return self.style
+    self._style = style
+    return self._style
 
   def set_related_to(self, tag_names):
     # Add to related_to_db
@@ -30,20 +30,20 @@ class Tag(object):
     return tag_names
 
   def get_status(self):
-    return self.status
+    return self._status
 
   def set_status(self, status):
-    self.status = status
-    return self.status
+    self._status = status
+    return self._status
 
   def get_counter(self):
-    return self.counter
+    return self._counter
 
   def _incr_counter(self, incr=1):
-    self.counter += incr
-    return self.counter
+    self._counter += incr
+    return self._counter
 
   def _decr_counter(self, decr=1):
-    self.counter -= decr
-    return self.counter
+    self._counter -= decr
+    return self._counter
 

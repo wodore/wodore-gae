a62e85bbfb09185f275f93fb7aebd2bb799e07fa
Tobias Burgherr <tburgherr@gmail.com>
2015-06-08 09:38:42 +0200

    Add user add from to collections.
    Modified collection list template and collection user list template.

--- main/control/collection_user.py
+++ main/control/collection_user.py
@@ -24,7 +24,12 @@ from main import app
 @app.route('/admin/collection/user')
 @auth.admin_required
 def collection_user_list():
-  col_usr_dbs, col_usr_cursor = model.CollectionUser.get_dbs(collection=util.param('collection',ndb.Key))
+  col_key = util.param('collection',ndb.Key)
+  if col_key:
+    col_db = col_key.get()
+  else:
+    col_db=None
+  col_usr_dbs, col_usr_cursor = model.CollectionUser.get_dbs(collection=col_key)
   #permissions = list(UserUpdateForm._permission_choices)
   #permissions += util.param('permissions', list) or []
   return flask.render_template(
@@ -32,62 +37,82 @@ def collection_user_list():
       html_class='collection-user-list',
       title='Collection User List',
       col_usr_dbs=col_usr_dbs,
+      col_db=col_db,
       next_url=util.generate_next_url(col_usr_cursor),
       api_url=None#flask.url_for('api.collection.list')
     )
 
 
 ###############################################################################
-# User Update
+# Add User
 ###############################################################################
-class CollectionUserUpdateForm(wtf.Form):
-  name = wtforms.StringField(
-      model.Collection.name._verbose_name,
+class CollectionUserAddForm(wtf.Form):
+  email = wtforms.StringField(
+      model.CollectionUser.user_email._verbose_name,
       [wtforms.validators.required(), wtforms.validators.length(min=3)]
     )
-  description = wtforms.TextAreaField(
-      model.Collection.description._verbose_name,
-      [wtforms.validators.optional()]
-    )
-  active = wtforms.BooleanField(model.Collection.active._verbose_name)
-  public = wtforms.BooleanField(model.Collection.public._verbose_name)
+
+  #permission = wtforms.RadioField(
+      #model.CollectionUser.permission._verbose_name,
+      #[wtforms.validators.required()],
+      #choices=['admin','write','read','none']
+    #)
+  permission = wtforms.SelectField(u'Permission',[wtforms.validators.required()],\
+      choices=[\
+      ("none","none"),\
+      ("read", "read"),\
+      ("write","write"),\
+      ("admin","admin"),\
+      ("creator","creator (don't user)")],default="read")
 
   def __init__(self, *args, **kwds):
-    super(CollectionUpdateForm, self).__init__(*args, **kwds)
+    super(CollectionUserAddForm, self).__init__(*args, **kwds)
 
 
-@app.route('/admin/collection/create/', methods=['GET', 'POST'])
-@app.route('/admin/collection/<col_key>/update/', methods=['GET', 'POST'])
+@app.route('/admin/collection/user/<col_key>/add/', methods=['GET', 'POST'])
 @auth.admin_required
-def collection_user_update(col_key=None):
-  update = False
-  col_db = True
+def collection_user_add(col_key=None):
   if col_key:
-    col_db = ndb.Key(urlsafe=col_key).get()
-    update = True
+    col_key = ndb.Key(urlsafe=col_key)
+    col_db = col_key.get()
   else:
-    col_db = model.Collection(name='')
-  if not col_db:
     flask.abort(404)
 
-  form = CollectionUpdateForm(obj=col_db)
+
+  #form = CollectionUserAddForm(obj=col_db)
+  form = CollectionUserAddForm()
   if form.validate_on_submit():
-    if update:
-      form.populate_obj(col_db)
-      col_db.put()
+    # get user key
+    user_keys = model.User.query(model.User.email==form.email.data).fetch(5,keys_only=True)
+    if not user_keys:
+      flask.flash("Unknown user.",'danger')
+      return flask.redirect(flask.url_for(
+        'collection_user_list', order='-modified', collection=col_key.urlsafe()
+        ))
+
+    if form.permission.data == 'none':
+      active = False
     else:
-      model.Collection.create(form.name.data,auth.current_user_key(),\
-          description=form.description.data, \
-          active=form.active.data, public=form.public.data)
+      active = True
+
+    model.Collection.add_users(col_key,user_keys,permission=form.permission.data,active=active)
+    #if update:
+      #form.populate_obj(col_db)
+      #col_db.put()
+    #else:
+      #model.Collection.create(form.name.data,auth.current_user_key(),\
+          #description=form.description.data, \
+          #active=form.active.data, public=form.public.data)
     return flask.redirect(flask.url_for(
-        'collection_list', order='-modified'
+        'collection_user_list', order='-modified', collection=col_key.urlsafe()
         ))
 
   return flask.render_template(
-      'collection/collection_update.html',
-      title=col_db.name or 'New Collection',
-      html_class='collection-update',
+      'collection/collection_user_add.html',
+      title=col_db.name or 'Add New User to Collection',
+      html_class='collection-user-add',
       form=form,
+      col_key=col_key,
       col_db=col_db,
       api_url=None#flask.url_for('api.user', col_key=col_db.key.urlsafe()) if col_db.key else ''
     )
